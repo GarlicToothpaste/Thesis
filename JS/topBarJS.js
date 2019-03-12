53b5c6e33141fba7ae1bef9789d9c10462e0bcb1
GarlicToothpaste <adrian_danao01@yahoo.com>
2019-03-12 23:56:00 +0800

    fixed drop-down menu

--- JS/topBarJS.js
+++ JS/topBarJS.js
@@ -2,15 +2,20 @@ $(document).ready(function(){
 	alert("Loaded")
 
 	$('[data-trigger="dropdown"]').on('mouseenter', function(){
-
 		var submenu = $(this).parent().find('.submenu');
-		//submenu.fadeIn(300);
 		submenu.addClass('active');
+		
+		//submenu.fadeIn(300);
+		
 
 		$('.profile-menu').on('mouseleave', function(){
+
 			$(this).find('.submenu').removeClass('active');
 			//submenu.fadeOut(300);
 		});
 
+		$('.algorithm-menu').on('mouseleave', function(){
+			$(this).find('.submenu').removeClass('active');
+		});
 	});
 })
