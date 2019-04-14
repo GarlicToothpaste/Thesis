74b0d0f0dd89bbcf27f0665d7ae12a9a838c83f7
GarlicToothpaste <adrian_danao01@yahoo.com>
2019-04-14 12:11:55 +0800

    Fixed front end

--- JS/topBarJS.js
+++ JS/topBarJS.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
-	alert("Loaded")
+	var isShown = false;
 
+	alert("Loaded")
+	//toEdit: edit '[data-trigger="dropdown"]' to specific
 	$('[data-trigger="dropdown"]').on('mouseenter', function(){
 		var submenu = $(this).parent().find('.submenu');
 		submenu.addClass('active');
 		
 		//submenu.fadeIn(300);
-		
 
 		$('.profile-menu').on('mouseleave', function(){
 
@@ -18,4 +19,35 @@ $(document).ready(function(){
 			$(this).find('.submenu').removeClass('active');
 		});
 	});
+
+
+	$('[data-trigger="clickable-dropdown').on('click' , function(){
+		
+		// alert("Clicked")
+		var dropdown = $(this).parent().find('.dropdown');
+
+		console.log(isShown);
+		//dropdown.addClass('active');
+		if( isShown === false){
+			$('.body-dropdown-menu').on('click' , function(){
+				//console.log("isShown === false");
+				//console.log($(this).find('.dropdown'));
+
+				$(this).find('.dropdown').addClass('active');
+				isShown = true;
+			})
+		}
+
+		else {
+			$('.body-dropdown-menu').on('click' , function(){
+				
+				//console.log("isShown === true");
+				//console.log($(this).find('.dropdown'));
+
+				$(this).find('.dropdown').removeClass('active');
+				isShown = false;	
+			})
+		}
+
+	})
 })
